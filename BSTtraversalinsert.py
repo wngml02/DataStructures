dde8fddbc1d279b44cd3ab34409f08e1d1e8d0ff
JUHEE <94836793+wngml02@users.noreply.github.com>
2022-05-04 20:27:20 +0900

    Add files via upload

---
+++ BSTtraversalinsert.py
@@ -0,0 +1,107 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.parent = self.left = self.right = None
+
+    def __str__(self):
+        return str(self.key)
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def preorder(self, v):
+        if v != None:
+            print(v, '', end='')
+            self.preorder(v.left)
+            self.preorder(v.right)
+
+    def inorder(self, v):
+        if v != None:
+            self.inorder(v.left)
+            print(v, '', end='')
+            self.inorder(v.right)
+
+    def postorder(self, v):
+        if v != None:
+            self.postorder(v.left)
+            self.postorder(v.right)
+            print(v, '', end='')
+
+    def find_loc(self, key):
+        if self.size == None:
+            return None
+        p = None
+        v = self.root
+        while v != None:
+            if v.key == key:
+                return v
+            elif v.key < key:
+                p = v
+                v = v.right
+            else:
+                p = v
+                v = v.left
+        return p
+
+    def search(self, key):
+        p = self.find_loc(key)
+        if p and p.key == key:
+            return p
+        else:
+            return None
+
+    def insert(self, key):
+        p = self.find_loc(key)
+        if p == None or p.key != key:
+            v = Node(key)
+            if p == None:
+                self.root = v
+            else:
+                v.parent = p
+                if p.key >= key:
+                    p.left = v
+                else:
+                    p.right = v
+            self.size = self.size + 1
+            return v
+        else:
+            print("key is already in the tree!")
+            return p
+        # key가 이미 트리에 있다면 에러 출력없이 None만 리턴!
+
+
+T = Tree()
+
+while True:
+    cmd = input().split()
+    if cmd[0] == 'insert':
+        v = T.insert(int(cmd[1]))
+        if v != None:
+            print("+ {0} is set into H".format(v.key))
+        else:
+            print(key, "is already in the tree!")
+    elif cmd[0] == 'search':
+        v = T.search(int(cmd[1]))
+        if v == None:
+            print("* {0} is not found!".format(cmd[1]))
+        else:
+            print(" * {0} is found!".format(cmd[1]))
+    elif cmd[0] == 'preorder':
+        T.preorder(T.root)
+        print()
+    elif cmd[0] == 'postorder':
+        T.postorder(T.root)
+        print()
+    elif cmd[0] == 'inorder':
+        T.inorder(T.root)
+        print()
+    elif cmd[0] == 'exit':
+        break
+    else:
+        print("* not allowed command. enter a proper command!")
